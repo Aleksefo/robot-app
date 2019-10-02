05665789c5bac6f6e47e9fe27b0413b2a39f3e5a
Tomi Kankaanpää <tomi.kankaanpaa@starship.co>
2019-10-02 15:05:00 +0300

    Make config cross-platform

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,7 @@
+import {Platform} from 'react-native';
+
 export default {
-  hostname: 'http://localhost:4000',
+  hostname:
+    Platform.OS === 'ios' ? 'http://localhost:4000' : 'http://10.0.2.2:4000',
   token: '00c25d96-ca8c-463d-8c1d-fadb39091ffe',
 };
